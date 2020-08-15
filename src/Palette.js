fb25dfda6dd4b6ceb12e2ca7cb3bc57dd9937d5c
Amal Khan <amalfkhan@gmail.com>
2020-08-15 08:52:33 -0700

    individual palette shell

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* nav */}
+        <div className="Palette-colours">
+          {/* Colour boxes */}
+        </div>
+        {/* footer */}
+      </div>
+    )
+  }
+}
